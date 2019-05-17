2c941e71ecd3af3f45cd7bff29076b649a941c54
Eliran <eliran.a.bg@gmail.com>
2019-05-16 21:06:15 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'todo-list', pathMatch: 'full' },
+  { path: 'todo-list', loadChildren: './views/todo-list/todo-list.module#TodoListPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/views/todo-list/todo-list.page.ts
@@ -0,0 +1,159 @@
+import { Component, OnInit } from '@angular/core';
+import {AlertController} from '@ionic/angular';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.page.html',
+  styleUrls: ['./todo-list.page.scss'],
+})
+export class TodoListPage implements OnInit {
+
+  todoList: [{
+    title: string;
+    description: string;
+    id: number;
+  }];
+  doneList: [{
+      title: string;
+      description: string;
+      id: number;
+  }];
+  id: number;
+
+  constructor(private alertController: AlertController) {
+      this.id = 0;
+      this.todoList = [
+          {title: 'TITLE', description: 'YYYYYYEEEEEEEEEDKJksdjfckjx sfksdbfkjsdn jsnfks d ksjndfks lsdf ksdiofnsdifnsd ln',  id: 1}
+      ];
+      this.doneList = [
+          {title: 'Example', description: 'YYYYYYEEEEEEEEEDKJksdjfckjx sfksdbfkjsdn jsnfks d ksjndfks lsdf ksdiofnsdifnsd ln',  id: 1}
+
+      ];
+
+      console.log('pls');
+  }
+
+  ngOnInit() {
+
+    // this.done = [{}];
+  }
+
+  getId(): number {
+    this.id = this.id + 1;
+    return this.id;
+  }
+
+    async delete(index: number) {
+        const alert = await this.alertController.create({
+            header: 'Are you sure you wish to delete?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary'
+                },
+                {
+                    text: 'Delete',
+                    handler: data => {
+                        // this.todoList.pop(index);
+                        this.todoList.splice(index, 1);
+                    }
+
+                }
+            ]
+        });
+
+        await alert.present();
+
+    }
+
+  async editTodo(index: number){
+      const alert = await this.alertController.create({
+          header: 'Add New Todo',
+          inputs: [
+              {
+                  title: 'title',
+                  type: 'text',
+                  id: 'title',
+                  value: this.todoList[index].title
+              },
+              {
+                  description: 'descr',
+                  type: 'text',
+                  id: 'descr',
+                  value: this.todoList[index].description
+              },
+          ],
+          buttons: [
+              {
+                  text: 'Cancel',
+                  role: 'cancel',
+                  cssClass: 'secondary'
+              },
+              {
+                  text: 'OK',
+                  handler: data => {
+                      this.todoList[index] = {
+                          title: data[0],
+                          description: data[1],
+                          id: this.todoList[index].id
+                      };
+                      // this.todoList.push({title: data[0], description: data[1], isdone: false, id: this.getId()});
+                  }
+
+              }
+          ]
+      });
+
+      await alert.present();
+
+  }
+
+  async addTodo() {
+      const alert = await this.alertController.create({
+          header: 'Add New Todo',
+          inputs: [
+              {
+                  title: 'title',
+                  type: 'text',
+                  id: 'title',
+                  placeholder: 'Title'
+              },
+              {
+                  description: 'descr',
+                  type: 'text',
+                  id: 'descr',
+                  placeholder: 'Details...'
+              },
+          ],
+          buttons: [
+              {
+                  text: 'Cancel',
+                  role: 'cancel',
+                  cssClass: 'secondary'
+              },
+              {
+                  text: 'OK',
+                  handler: data => {
+                    this.todoList.push({title: data[0], description: data[1],  id: this.getId()});
+                  }
+
+              }
+          ]
+      });
+
+      await alert.present();
+  }
+
+  done(index: number) {
+
+    let temp = this.todoList.splice(index, 1);
+    console.log(temp);
+      this.doneList.push(temp[0]);
+
+      console.log(this.doneList);
+
+
+
+  }
+}
